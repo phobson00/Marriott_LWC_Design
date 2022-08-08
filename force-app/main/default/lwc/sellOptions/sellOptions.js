a5cc47934cd69fcec540d6bfe689a5d437a719f2
Peyton Hobson <peyhobso@publicisgroupe.net>
2022-08-08 14:58:33 -0600

    Progressed on styling menu

--- force-app/main/default/lwc/sellOptions/sellOptions.js
+++ force-app/main/default/lwc/sellOptions/sellOptions.js
@@ -18,14 +18,24 @@ export default class SellOptions extends LightningElement {
 
 	changeBtnColor(event) {
 
-		let btn = this.template.querySelector('.content-link-selected');
+		// let btn = this.template.querySelector('.content-btn-selected');
 
-		if(btn) {
-			btn.classList.remove('content-link-selected');
-			btn.classList.add('content-link')
-		}
+		// if(btn) {
+		// 	btn.classList.remove('content-btn-selected');
+		// 	btn.classList.add('content-btn')
+		// }
 
-		event.target.classList.remove('content-link');
-		event.target.classList.add('content-link-selected');
+		// event.target.parentElement.classList.remove('content-btn');
+		// event.target.parentElement.classList.add('content-btn-selected');
+
+		// let link = this.template.querySelector('.content-link-selected');
+
+		// if(link) {
+		// 	link.classList.remove('content-link-selected');
+		// 	link.classList.add('content-link')
+		// }
+
+		// event.target.classList.remove('content-link');
+		// event.target.classList.add('content-link-selected');
 	}
 }
