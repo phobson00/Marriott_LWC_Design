5f7cd3efcc390d898e5c8886ecfd51808371fc48
Peyton Hobson <peyhobso@publicisgroupe.net>
2022-08-08 09:33:23 -0600

    Styled sellOptions Box

--- force-app/main/default/lwc/sellOptions/sellOptions.js
+++ force-app/main/default/lwc/sellOptions/sellOptions.js
@@ -2,9 +2,30 @@ import { api, LightningElement } from 'lwc';
 
 export default class SellOptions extends LightningElement {
 
-	@api
-	label;
+	showDropdown(event) {
+			var dropdowns = this.template.querySelectorAll('.dropdown-content');
+			var i;
+			for (i = 0; i < dropdowns.length; i++) {
+			  var openDropdown = dropdowns[i];
+			  if (openDropdown.style.display == "flex") {
+				openDropdown.style.display = "none";
+			  }
+			  else {
+				openDropdown.style.display = "flex";
+			  }
+			}
+	}
 
-	@api
-	firstOption;
+	changeBtnColor(event) {
+
+		let btn = this.template.querySelector('.content-link-selected');
+
+		if(btn) {
+			btn.classList.remove('content-link-selected');
+			btn.classList.add('content-link')
+		}
+
+		event.target.classList.remove('content-link');
+		event.target.classList.add('content-link-selected');
+	}
 }
