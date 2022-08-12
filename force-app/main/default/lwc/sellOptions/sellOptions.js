011248c42a0de3ba32fba97ed242ae18729741ac
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-08-12 09:28:39 -0600

    Implemented dropdown functionality in JS

--- force-app/main/default/lwc/sellOptions/sellOptions.js
+++ force-app/main/default/lwc/sellOptions/sellOptions.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class SellOptions extends LightningElement {}
+export default class SellOptions extends LightningElement {
+
+	dropMenu() {
+
+		const dropTrigger = this.template.querySelector('.slds-dropdown-trigger_click');
+
+		if(dropTrigger.classList.contains('slds-is-open')) {
+			dropTrigger.classList.remove('slds-is-open')
+		}
+		else {
+			dropTrigger.classList.add('slds-is-open')
+		}
+	}
+
+
+}
