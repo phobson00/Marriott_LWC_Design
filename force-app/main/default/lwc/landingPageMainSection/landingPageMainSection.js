530ad096a52970bce8c67fbcbd5faa083e8c1dc4
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-08-17 07:49:56 -0600

    Refactored main functional section of landing page to be its own component

--- force-app/main/default/lwc/landingPageMainSection/landingPageMainSection.js
+++ force-app/main/default/lwc/landingPageMainSection/landingPageMainSection.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { track, LightningElement } from "lwc";
 
-export default class LandingPageMainSection extends LightningElement {}
+export default class LandingPageMainSection extends LightningElement {
+  @track
+  placeholder;
+
+  cityTabHandler() {
+    this.placeholder = "Enter Location";
+  }
+
+  hotelTabHandler() {
+    this.placeholder = "Enter Hotel Name";
+  }
+}
