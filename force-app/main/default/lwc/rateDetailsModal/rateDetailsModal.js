da3e86bb779a986865e98656f9d07a721a86a34f
Daryn Capps <darcapps@publicisgroupe.net>
2022-08-17 14:28:40 -0600

    added js logic for opening/closing

--- force-app/main/default/lwc/rateDetailsModal/rateDetailsModal.js
+++ force-app/main/default/lwc/rateDetailsModal/rateDetailsModal.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class RateDetailsModal extends LightningElement {}
+export default class RateDetailsModal extends LightningElement {
+    @track isShowModal = false;
+
+    showModalBox() {  
+        this.isShowModal = true;
+    }
+
+    hideModalBox() {  
+        this.isShowModal = false;
+    }
+}
