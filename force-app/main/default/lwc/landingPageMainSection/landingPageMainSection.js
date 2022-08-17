0f3d4847ff07cf4bdc5a319cf72ec95646793723
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-08-17 08:20:43 -0600

    Made columns for landing page main section dynamic

--- force-app/main/default/lwc/landingPageMainSection/landingPageMainSection.js
+++ force-app/main/default/lwc/landingPageMainSection/landingPageMainSection.js
@@ -1,8 +1,14 @@
-import { track, LightningElement } from "lwc";
+import { track, LightningElement, api } from "lwc";
 
 export default class LandingPageMainSection extends LightningElement {
   @track
   placeholder;
+
+  @api
+  column1;
+
+  @api
+  column2;
 
   cityTabHandler() {
     this.placeholder = "Enter Location";
