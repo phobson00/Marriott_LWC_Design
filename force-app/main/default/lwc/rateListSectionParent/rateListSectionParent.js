bf4a92854abdb0c2943fa1359ef34e259abd60e2
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-08-18 10:36:04 -0600

    Added function to dynamically add and remove map

--- force-app/main/default/lwc/rateListSectionParent/rateListSectionParent.js
+++ force-app/main/default/lwc/rateListSectionParent/rateListSectionParent.js
@@ -5,4 +5,14 @@ export default class RateListSectionParent extends LightningElement {
   image1 = marriott_images + "/images/marriott_image_1.jpg";
   image2 = marriott_images + "/images/marriott_image_2.jpg";
   image3 = marriott_images + "/images/marriott_image_3.jpg";
+
+  showMap = false;
+
+  displayMap() {
+    if (this.showMap) {
+      this.showMap = false;
+    } else {
+      this.showMap = true;
+    }
+  }
 }
