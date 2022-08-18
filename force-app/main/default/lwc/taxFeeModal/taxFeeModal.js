ca801b47282f1c220c0247c9beb9103ba718d505
Daryn Capps <darcapps@publicisgroupe.net>
2022-08-18 09:13:36 -0600

    added blank modal

---
+++ force-app/main/default/lwc/taxFeeModal/taxFeeModal.js
@@ -0,0 +1,13 @@
+import { LightningElement, track } from 'lwc';
+
+export default class TaxFeeModal extends LightningElement {
+    @track isShowModal = false;
+
+    showModalBox() {  
+        this.isShowModal = true;
+    }
+
+    hideModalBox() {  
+        this.isShowModal = false;
+    }
+}
