c4fc8453192fc729e489a0979415f419867ff3fc
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-08-22 15:22:06 -0600

    Added LWC workaround and home icon

--- force-app/main/default/lwc/parentWithTabs/parentWithTabs.js
+++ force-app/main/default/lwc/parentWithTabs/parentWithTabs.js
@@ -11,4 +11,48 @@ export default class ParentWithTabs extends LightningElement {
   hotelTabHandler() {
     this.placeholder = "Enter Hotel Name";
   }
+
+  connectedCallback() {
+    this.invokeWorkspaceAPI("isConsoleNavigation").then((isConsole) => {
+      if (isConsole) {
+        this.invokeWorkspaceAPI("getFocusedTabInfo").then((focusedTab) => {
+          this.invokeWorkspaceAPI("setTabLabel", {
+            tabId: focusedTab.tabId,
+            label: "  Landing Page"
+          });
+          this.invokeWorkspaceAPI("setTabIcon", {
+            tabId: focusedTab.tabId,
+            icon: "utility:home"
+          });
+          console.log(focusedTab.tabId);
+        });
+      }
+    });
+  }
+
+  invokeWorkspaceAPI(methodName, methodArgs) {
+    return new Promise((resolve, reject) => {
+      const apiEvent = new CustomEvent("internalapievent", {
+        bubbles: true,
+        composed: true,
+        cancelable: false,
+        detail: {
+          category: "workspaceAPI",
+          methodName: methodName,
+          methodArgs: methodArgs,
+          callback: (err, response) => {
+            if (err) {
+              console.log("err");
+              return reject(err);
+            }
+
+            console.log("response " + response);
+            return resolve(response);
+          }
+        }
+      });
+
+      window.dispatchEvent(apiEvent);
+    });
+  }
 }
