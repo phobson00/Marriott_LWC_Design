7730c9f0056fd84c9adaf3082b2baf49f2670592
Daryn Capps <darcapps@publicisgroupe.net>
2022-08-22 09:50:53 -0600

    fixed text, size and added table

--- force-app/main/default/lwc/taxFeeModal/taxFeeModal.js
+++ force-app/main/default/lwc/taxFeeModal/taxFeeModal.js
@@ -1,5 +1,28 @@
 import { LightningElement, track } from 'lwc';
 
+const columns = [
+    { label: 'ID', fieldName: 'id' },
+    { label: 'Tax/Fee Name', fieldName: 'taxFee'},
+    { label: 'Effective Date', fieldName: 'date'},
+    { label: 'Indicators', fieldName: 'indicators'},
+    { label: 'Amount', fieldName: 'amount'},
+    { label: 'Charge', fieldName: 'charge'},
+    { label: 'Frequency', fieldName: 'frequency'}
+];
+
+const data = [
+    {
+        id: 1,
+        taxFee: "City / Municpal Tax",
+        date: "15DEC15",
+        indicators: "E",
+        amount: 2.5, 
+        charge: "R",
+        frequecy: "N"
+    }
+];
+
+
 export default class TaxFeeModal extends LightningElement {
     @track isShowModal = false;
 
@@ -10,4 +33,11 @@ export default class TaxFeeModal extends LightningElement {
     hideModalBox() {  
         this.isShowModal = false;
     }
+
+    columns = columns;
+    data = data;
+
 }
+
+
+
