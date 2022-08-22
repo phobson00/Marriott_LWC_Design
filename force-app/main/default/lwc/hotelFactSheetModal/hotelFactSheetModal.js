b78ddf4b7709c9a8203409b58497e1169b4fed1a
Daryn Capps <darcapps@publicisgroupe.net>
2022-08-22 10:34:00 -0600

    added html and js

--- force-app/main/default/lwc/hotelFactSheetModal/hotelFactSheetModal.js
+++ force-app/main/default/lwc/hotelFactSheetModal/hotelFactSheetModal.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HotelFactSheetModal extends LightningElement {}
+export default class HotelFactSheetModal extends LightningElement {
+    @track isShowModal = false;
+
+    showModalBox() {  
+        this.isShowModal = true;
+    }
+
+    hideModalBox() {  
+        this.isShowModal = false;
+    }
+
+}
