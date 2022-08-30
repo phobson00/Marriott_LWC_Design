b28995e8b07aa9b15156dcfd1064e64d628183a8
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-08-30 11:30:09 -0600

    Added dynamic tabs to hotel list

--- force-app/main/default/lwc/hotelListDynamicTabs/hotelListDynamicTabs.js
+++ force-app/main/default/lwc/hotelListDynamicTabs/hotelListDynamicTabs.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from "lwc";
 
-export default class HotelListDynamicTabs extends LightningElement {}
+export default class HotelListDynamicTabs extends LightningElement {
+  @api
+  numberOfTabs;
+
+  tabs = [];
+
+  allTabs = [
+    {
+      text: "Special Rates",
+      value: "Rates",
+      id: 1
+    },
+    {
+      text: "Use Points",
+      value: "Rates",
+      id: 2
+    },
+    {
+      text: "Standard Rates",
+      value: "Rates",
+      id: 3
+    },
+    {
+      text: "Prepay and Save",
+      value: "Rates",
+      id: 4
+    },
+    {
+      text: "Deals and Packages",
+      value: "Rates",
+      id: 5
+    }
+  ];
+
+  connectedCallback() {
+    for (let i = 0; i < this.numberOfTabs; i++) {
+      this.tabs.push(this.allTabs[i]);
+    }
+
+    console.log(this.tabs[0].text);
+  }
+}
