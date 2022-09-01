c92878e439c511097bcffffedf62301117a2fafd
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-01 09:11:00 -0600

    Added booking cart empty template

--- force-app/main/default/lwc/bookingCartDropdown/bookingCartDropdown.js
+++ force-app/main/default/lwc/bookingCartDropdown/bookingCartDropdown.js
@@ -1,3 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class BookingCartDropdown extends LightningElement {}
+export default class BookingCartDropdown extends LightningElement {
+  dropMenu() {
+    const dropTrigger = this.template.querySelector(
+      ".slds-dropdown-trigger_click"
+    );
+
+    if (dropTrigger.classList.contains("slds-is-open")) {
+      dropTrigger.classList.remove("slds-is-open");
+    } else {
+      dropTrigger.classList.add("slds-is-open");
+    }
+  }
+}
