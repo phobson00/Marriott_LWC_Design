4548b5dc73f8129aa5b0dc03778587a21139bf80
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-01 11:21:26 -0600

    Added components without styling in bookingCartPageItem

--- force-app/main/default/lwc/bookingCartPageItem/bookingCartPageItem.js
+++ force-app/main/default/lwc/bookingCartPageItem/bookingCartPageItem.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import marriott_images from "@salesforce/resourceUrl/Marriott_Images";
 
-export default class BookingCartPageItem extends LightningElement {}
+export default class BookingCartPageItem extends LightningElement {
+  image = marriott_images + "/images/marriott_image_1.jpg";
+}
