3b7b90d0179d097588679834ec962e623d172d6f
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-01 13:03:40 -0600

    Finished styling of cart item

--- force-app/main/default/lwc/bookingCartPageItem/bookingCartPageItem.js
+++ force-app/main/default/lwc/bookingCartPageItem/bookingCartPageItem.js
@@ -3,4 +3,12 @@ import marriott_images from "@salesforce/resourceUrl/Marriott_Images";
 
 export default class BookingCartPageItem extends LightningElement {
   image = marriott_images + "/images/marriott_image_1.jpg";
+
+  showSummary = false;
+
+  summaryHandler() {
+    return this.showSummary
+      ? (this.showSummary = false)
+      : (this.showSummary = true);
+  }
 }
