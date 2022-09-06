7f0bba0d9a0ab4c17293ca8de243c0457204a3e4
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-06 10:12:17 -0600

    Added other generic dropdowns

--- force-app/main/default/lwc/reservationsDropdown/reservationsDropdown.js
+++ force-app/main/default/lwc/reservationsDropdown/reservationsDropdown.js
@@ -1,3 +1,6 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from "lwc";
 
-export default class ReservationsDropdown extends LightningElement {}
+export default class ReservationsDropdown extends LightningElement {
+  @api
+  label;
+}
