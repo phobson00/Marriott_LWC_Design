9115c0decc6ccc0a3af6922ea095f33f97005193
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-06 11:10:22 -0600

    Finished travel planner dropdown

--- force-app/main/default/lwc/travelPlannerDropdown/travelPlannerDropdown.js
+++ force-app/main/default/lwc/travelPlannerDropdown/travelPlannerDropdown.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class TravelPlannerDropdown extends LightningElement {}
+export default class TravelPlannerDropdown extends LightningElement {
+  showMenu = false;
+
+  dropMenu() {
+    return this.showMenu ? (this.showMenu = false) : (this.showMenu = true);
+  }
+}
