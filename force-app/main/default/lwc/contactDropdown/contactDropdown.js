2e55437715fb1b2df655b448cb13cb751b73fa06
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-06 10:02:32 -0600

    Added dyanmic tab content change in contact dropdown

--- force-app/main/default/lwc/contactDropdown/contactDropdown.js
+++ force-app/main/default/lwc/contactDropdown/contactDropdown.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class ContactDropdown extends LightningElement {}
+export default class ContactDropdown extends LightningElement {
+  showInput = false;
+
+  tabClick(e) {
+    const allTabs = this.template.querySelectorAll("ul>li");
+    allTabs.forEach((elm, idx) => {
+      elm.classList.remove("slds-is-active");
+    });
+    e.currentTarget.classList.add("slds-is-active");
+
+    if (e.currentTarget.title === "Item One") {
+      this.showInput = false;
+    } else {
+      this.showInput = true;
+    }
+  }
+}
