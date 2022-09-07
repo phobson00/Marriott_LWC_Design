299e2ac255e0ec1ed6b1211eb2a21f38134d3d94
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-07 13:27:10 -0600

    Added dynamic spacing of tabs

--- force-app/main/default/lwc/hotelListDynamicTabs/hotelListDynamicTabs.js
+++ force-app/main/default/lwc/hotelListDynamicTabs/hotelListDynamicTabs.js
@@ -1,47 +1,73 @@
+/* eslint-disable guard-for-in */
 import { LightningElement } from "lwc";
 
 export default class HotelListDynamicTabs extends LightningElement {
   numberOfTabs = 2;
 
-  addButton() {
+  tabClass =
+    "slds-tabs_default__item slds-border_right slds-border_left slds-size_1-of-2";
+
+  isThreeTab = false;
+  isFourTab = false;
+  isFiveTab = false;
+
+  addTab() {
     if (this.numberOfTabs < 5) {
       this.numberOfTabs++;
-      this.columnClass = this.columnClasses[this.numberOfTabs - 2];
     }
 
     this.setIsTabAdd();
   }
 
-  removeButton() {
+  removeTab() {
     if (this.numberOfTabs > 2) {
       this.numberOfTabs--;
-      this.columnClass = this.columnClasses[this.numberOfTabs - 2];
     }
 
     this.setIsTabRemove();
   }
 
   setIsTabAdd() {
-    if (this.numberOfTabs > 2) {
+    if (this.numberOfTabs == 3) {
       this.isThreeTab = true;
-    }
-    if (this.numberOfTabs > 3) {
+      this.tabClass = this.tabClass.replace(
+        "slds-size_1-of-2",
+        "slds-size_1-of-3"
+      );
+    } else if (this.numberOfTabs == 4) {
       this.isFourTab = true;
-    }
-    if (this.numberOfTabs > 4) {
+      this.tabClass = this.tabClass.replace(
+        "slds-size_1-of-3",
+        "slds-size_1-of-4"
+      );
+    } else if (this.numberOfTabs == 5) {
       this.isFiveTab = true;
+      this.tabClass = this.tabClass.replace(
+        "slds-size_1-of-4",
+        "slds-size_1-of-5"
+      );
     }
   }
 
   setIsTabRemove() {
-    if (this.numberOfTabs < 5) {
+    if (this.numberOfTabs == 4) {
       this.isFiveTab = false;
-    }
-    if (this.numberOfTabs < 4) {
+      this.tabClass = this.tabClass.replace(
+        "slds-size_1-of-5",
+        "slds-size_1-of-4"
+      );
+    } else if (this.numberOfTabs == 3) {
       this.isFourTab = false;
-    }
-    if (this.numberOfTabs < 3) {
+      this.tabClass = this.tabClass.replace(
+        "slds-size_1-of-4",
+        "slds-size_1-of-3"
+      );
+    } else if (this.numberOfTabs == 2) {
       this.isThreeTab = false;
+      this.tabClass = this.tabClass.replace(
+        "slds-size_1-of-3",
+        "slds-size_1-of-2"
+      );
     }
   }
 }
