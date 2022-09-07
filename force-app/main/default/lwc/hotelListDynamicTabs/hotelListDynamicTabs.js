2d1c1f5f5c01eefe1f8abd096438ec8f04d29567
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-07 14:13:41 -0600

    Simplified JS code for tabs

--- force-app/main/default/lwc/hotelListDynamicTabs/hotelListDynamicTabs.js
+++ force-app/main/default/lwc/hotelListDynamicTabs/hotelListDynamicTabs.js
@@ -30,44 +30,30 @@ export default class HotelListDynamicTabs extends LightningElement {
   setIsTabAdd() {
     if (this.numberOfTabs == 3) {
       this.isThreeTab = true;
-      this.tabClass = this.tabClass.replace(
-        "slds-size_1-of-2",
-        "slds-size_1-of-3"
-      );
     } else if (this.numberOfTabs == 4) {
       this.isFourTab = true;
-      this.tabClass = this.tabClass.replace(
-        "slds-size_1-of-3",
-        "slds-size_1-of-4"
-      );
     } else if (this.numberOfTabs == 5) {
       this.isFiveTab = true;
-      this.tabClass = this.tabClass.replace(
-        "slds-size_1-of-4",
-        "slds-size_1-of-5"
-      );
     }
+
+    this.tabClass = this.tabClass.replace(
+      this.numberOfTabs - 1,
+      this.numberOfTabs
+    );
   }
 
   setIsTabRemove() {
     if (this.numberOfTabs == 4) {
       this.isFiveTab = false;
-      this.tabClass = this.tabClass.replace(
-        "slds-size_1-of-5",
-        "slds-size_1-of-4"
-      );
     } else if (this.numberOfTabs == 3) {
       this.isFourTab = false;
-      this.tabClass = this.tabClass.replace(
-        "slds-size_1-of-4",
-        "slds-size_1-of-3"
-      );
     } else if (this.numberOfTabs == 2) {
       this.isThreeTab = false;
-      this.tabClass = this.tabClass.replace(
-        "slds-size_1-of-3",
-        "slds-size_1-of-2"
-      );
     }
+
+    this.tabClass = this.tabClass.replace(
+      this.numberOfTabs + 1,
+      this.numberOfTabs
+    );
   }
 }
