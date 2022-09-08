02627190f47c7d026637b02cafaeb54cd5de939f
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-08 09:21:36 -0600

    Fixed issue with bracket

--- force-app/main/default/lwc/reviewDetailsParent/reviewDetailsParent.js
+++ force-app/main/default/lwc/reviewDetailsParent/reviewDetailsParent.js
@@ -1,46 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class ReviewDetailsParent extends LightningElement {
+  connectedCallback() {
+    this.invokeWorkspaceAPI("isConsoleNavigation").then((isConsole) => {
+      if (isConsole) {
+        this.invokeWorkspaceAPI("getFocusedTabInfo").then((focusedTab) => {
+          this.invokeWorkspaceAPI("setTabLabel", {
+            tabId: focusedTab.tabId,
+            label: "ResApp Review Reservation Details"
+          });
+          this.invokeWorkspaceAPI("setTabIcon", {
+            tabId: focusedTab.tabId,
+            icon: "utility:record_update"
+          });
+        });
+      }
+    });
+  }
 
-	connectedCallback() {
-		this.invokeWorkspaceAPI("isConsoleNavigation").then((isConsole) => {
-		  if (isConsole) {
-			this.invokeWorkspaceAPI("getFocusedTabInfo").then((focusedTab) => {
-			  this.invokeWorkspaceAPI("setTabLabel", {
-				tabId: focusedTab.tabId,
-				label: "ResApp Review Reservation Details"
-			  });
-			  this.invokeWorkspaceAPI("setTabIcon", {
-				tabId: focusedTab.tabId,
-				icon: "utility:record_update"
-			  });
-			});
-		  }
-		});
-	  }
-	
-	  invokeWorkspaceAPI(methodName, methodArgs) {
-		return new Promise((resolve, reject) => {
-		  const apiEvent = new CustomEvent("internalapievent", {
-			bubbles: true,
-			composed: true,
-			cancelable: false,
-			detail: {
-			  category: "workspaceAPI",
-			  methodName: methodName,
-			  methodArgs: methodArgs,
-			  callback: (err, response) => {
-				if (err) {
-				  return reject(err);
-				}
-	
-				return resolve(response);
-			  }
-			}
-		  });
-	
-		  window.dispatchEvent(apiEvent);
-		});
-	  }
-	}
+  invokeWorkspaceAPI(methodName, methodArgs) {
+    return new Promise((resolve, reject) => {
+      const apiEvent = new CustomEvent("internalapievent", {
+        bubbles: true,
+        composed: true,
+        cancelable: false,
+        detail: {
+          category: "workspaceAPI",
+          methodName: methodName,
+          methodArgs: methodArgs,
+          callback: (err, response) => {
+            if (err) {
+              return reject(err);
+            }
+
+            return resolve(response);
+          }
+        }
+      });
+
+      window.dispatchEvent(apiEvent);
+    });
+  }
 }
