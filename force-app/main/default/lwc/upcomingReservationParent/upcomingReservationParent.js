28aae40b4936f9667372d3eac16111772d611a8a
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-12 08:37:29 -0600

    Added bottom border to bold text area

--- force-app/main/default/lwc/upcomingReservationParent/upcomingReservationParent.js
+++ force-app/main/default/lwc/upcomingReservationParent/upcomingReservationParent.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class UpcomingReservationParent extends LightningElement {}
+export default class UpcomingReservationParent extends LightningElement {
+  connectedCallback() {
+    this.invokeWorkspaceAPI("isConsoleNavigation").then((isConsole) => {
+      if (isConsole) {
+        this.invokeWorkspaceAPI("getFocusedTabInfo").then((focusedTab) => {
+          this.invokeWorkspaceAPI("setTabLabel", {
+            tabId: focusedTab.tabId,
+            label: "ResApp Upcoming Reservation"
+          });
+          this.invokeWorkspaceAPI("setTabIcon", {
+            tabId: focusedTab.tabId,
+            icon: "utility:record_update"
+          });
+        });
+      }
+    });
+  }
+
+  invokeWorkspaceAPI(methodName, methodArgs) {
+    return new Promise((resolve, reject) => {
+      const apiEvent = new CustomEvent("internalapievent", {
+        bubbles: true,
+        composed: true,
+        cancelable: false,
+        detail: {
+          category: "workspaceAPI",
+          methodName: methodName,
+          methodArgs: methodArgs,
+          callback: (err, response) => {
+            if (err) {
+              return reject(err);
+            }
+
+            return resolve(response);
+          }
+        }
+      });
+
+      window.dispatchEvent(apiEvent);
+    });
+  }
+}
