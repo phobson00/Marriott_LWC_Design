9691a6bebaac0c69d9df924dec49050e0ef62889
peyton.hobson@publicissapient.com <peyton.hobson@publicissapient.com>
2022-09-19 10:12:31 -0700

    Added caller type dropdown

--- force-app/main/default/lwc/guestInformationDropdown/guestInformationDropdown.js
+++ force-app/main/default/lwc/guestInformationDropdown/guestInformationDropdown.js
@@ -3,6 +3,8 @@ import { LightningElement } from "lwc";
 export default class GuestInformationDropdown extends LightningElement {
   showDropdown = false;
 
+  showMenu = false;
+
   dropdownHandler(event) {
     if (this.showDropdown) {
       this.showDropdown = false;
@@ -10,6 +12,16 @@ export default class GuestInformationDropdown extends LightningElement {
     } else {
       this.showDropdown = true;
       event.target.iconName = "utility:chevronup";
+    }
+  }
+
+  showDrop() {
+    const drop = this.template.querySelector(".slds-dropdown-trigger");
+
+    if (drop.classList.contains("slds-is-open")) {
+      drop.classList.remove("slds-is-open");
+    } else {
+      drop.classList.add("slds-is-open");
     }
   }
 }
